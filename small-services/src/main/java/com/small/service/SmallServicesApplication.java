5ce9066d7a7308c14ce71f43743095105eeb89d6
Musa Daniel <jtccontractsservicesng@yahoo.com>
2025-10-28 21:23:36 +0100

    all micro service eureka client discovered by eureka server.

--- small-services/src/main/java/com/small/service/SmallServicesApplication.java
+++ small-services/src/main/java/com/small/service/SmallServicesApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class SmallServicesApplication {
-
+// me
 	public static void main(String[] args) {
 		SpringApplication.run(SmallServicesApplication.class, args);
 	}
