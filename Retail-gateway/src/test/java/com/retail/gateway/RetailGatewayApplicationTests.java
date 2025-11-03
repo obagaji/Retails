5b1701f8b13e5802b3f1c908b2c7c9c3de8e6278
Musa Daniel <jtccontractsservicesng@yahoo.com>
2025-11-03 06:24:06 +0100

    gateway access

---
+++ Retail-gateway/src/test/java/com/retail/gateway/RetailGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retail.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
